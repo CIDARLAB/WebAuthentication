2e0db0280702c522a84dc5a3ca8641cae6421a14
Ernst Oberortner <e.oberortner@gmail.com>
2014-12-16 09:51:23 -0500

    initial version of WebAuthenticatio framework

---
+++ src/main/java/org/cidarlab/web/AuthenticationConstants.java
@@ -0,0 +1,21 @@
+package org.cidarlab.web;
+
+/**
+ * The AuthenticationConstants class is an 
+ * enumeration of public static non-mutable 
+ * members for the data exchange between 
+ * the Front-End and the Back-End of Web applications.
+ * 
+ * @author Ernst Oberortner
+ *
+ */
+public class AuthenticationConstants {
+
+	public static final String COMMAND = "command";
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "password";
+	
+	public static final String SIGNUP = "signup";
+	public static final String LOGIN = "login";
+	public static final String LOGOUT = "logout";
+}
---
+++ src/main/java/org/cidarlab/web/AuthenticationServlet.java
@@ -0,0 +1,248 @@
+package org.cidarlab.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+/**
+ * The AuthenticationServlet serves POST requests regarding
+ * SIGNUP, LOGIN, and LOGOUT for Web Applications.
+ * It also servers GET requests through forward the user 
+ * to the index.html page.
+ * 
+ * @author Ernst Oberortner
+ */
+public class AuthenticationServlet 
+	extends HttpServlet {
+
+	private static final long serialVersionUID = -1579220291590687064L;
+	
+	private static final String USER_DB_NAME = "CIDAR";
+	private static org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger("AuthenticationServlet");
+	
+	
+	// a reference to an instance 
+	// of the CIDAR authenticator
+	private Authenticator auth;
+	
+	@Override
+	public void init(ServletConfig config) 
+			throws ServletException {
+		
+	    super.init(config);
+	    
+	    this.auth = new Authenticator(USER_DB_NAME);
+	    
+	    // set a system property such that Simple Logger will include timestamp
+        System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
+        // set a system property such that Simple Logger will include timestamp in the given format
+        System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "dd-MM-yy HH:mm:ss");
+
+        // set minimum log level for SLF4J Simple Logger at warn
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
+        
+        LOGGER.warn("[AuthenticationServlet] loaded!");	    
+	}
+	
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    	JSONObject jsonResponse = new JSONObject();
+    	
+        try {
+        	
+        	// get the username and password parameter values 
+        	// from the request
+        	String command = request.getParameter(AuthenticationConstants.COMMAND);
+        	String username = request.getParameter(AuthenticationConstants.USERNAME);
+        	String password = request.getParameter(AuthenticationConstants.PASSWORD);
+        	
+        	/*
+        	 * SIGNUP Request
+        	 */
+        	if(AuthenticationConstants.SIGNUP.equals(command)) {
+        		
+        		// register the user
+            	this.auth.register(username, password);
+            	
+            	// we automatically login the user, 
+            	// i.e. we do some session management 
+            	this.login(request, response, username);
+
+            /*
+             * LOGIN Request
+             */
+        	} else if(AuthenticationConstants.LOGIN.equals(command)) {
+        		
+        		// first, we check if the user exists and 
+        		// if the passwords match up
+        		boolean bLogin = this.auth.login(username, password);
+        		
+        		if(bLogin) {
+        			
+        			// invalidate the session
+        			this.invalidateSession(request);
+        			
+            		// login the user including session management
+                	this.login(request, response, username);
+        		}
+        		
+        	/*
+        	 *  LOGOUT Request
+        	 */
+        	} else if(AuthenticationConstants.LOGOUT.equals(command)) {
+        		
+        		// we just invalidate the user's session
+        		this.invalidateSession(request);
+        		
+        	/*
+        	 * Invalid Request	
+        	 */
+            } else {
+            	LOGGER.warn("Invalid login! user: " + username + ", password: " + password);
+            	throw new AuthenticationException("Invalid Request!");
+            }
+        	
+        	jsonResponse.put("status", "good");
+        	
+        } catch(Exception e) {
+        	
+    		LOGGER.warn(e.getMessage());
+    		
+    		jsonResponse.put("status", "exception");
+    		jsonResponse.put("result", e.getMessage());
+        } 
+
+        /*
+         * write the response
+         */
+    	PrintWriter out = response.getWriter();
+    	response.setContentType("application/json");
+        
+    	out.write(jsonResponse.toString());
+    	
+    	out.flush();
+        out.close();
+    }
+    
+    /**
+     * The invalidateSession(HttpServletRequest) method removes 
+     * all attributes from the request's session, i.e. user and eugenelab
+     * information.
+     * 
+     * @param request
+     */
+    private void invalidateSession(HttpServletRequest request) {
+    	
+    	if(null != request && null != request.getSession()) {
+    		
+			// the session expires immediately
+			request.getSession().setMaxInactiveInterval(1);
+
+			// we remove the user information
+			request.getSession().removeAttribute("user");
+			
+			// we remove the EugeneLab information
+			request.getSession().removeAttribute("eugenelab");
+			
+			// finally, we invalidate it
+    		request.getSession().invalidate();
+    	}
+    }
+    
+    private void login(HttpServletRequest request, HttpServletResponse response, String user) {
+
+		/*-------------------------------
+		 * VALID AUTHENTICATION 
+		 *-------------------------------*/  
+		
+		// we create a session
+		HttpSession session = request.getSession(true);
+		
+		// put the username into it
+        session.setAttribute("user", user);
+
+        // a session expires after 60 mins
+        session.setMaxInactiveInterval(60 * 60);
+        
+        // also, we set two cookies
+        // - the first cookie indicates of the user is authenticated
+        // - the second cookie contains user information
+        Cookie authenticateCookie = new Cookie("eugenelab", "authenticated");
+        Cookie userCookie = new Cookie("user", user);
+        authenticateCookie.setMaxAge(60 * 60); //cookie lasts for an hour
+        
+        // add both cookies to the response
+        response.addCookie(authenticateCookie);
+        response.addCookie(userCookie);
+    }
+    	
+
+
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processGetRequest(request, response);
+    }
+
+    /**
+     * Processes requests for HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processGetRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+        PrintWriter out = response.getWriter();
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.sendRedirect("index.html");
+        
+        
+        out.flush();
+        out.close();
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "CIDAR-Lab Servlet for SIGNUP, LOGIN, and LOGOUT for Web Applications";
+    }
+
+}
